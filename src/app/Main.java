88d7d25047e772aea5182a8ec9053a6906007444
Riccardo Gulin <riccardo.gulin@gmail.com>
2023-05-04 11:09:18 +0200

    Adds loops examples

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,31 +3,99 @@ package app;
 public class Main {
 
 	public static void main(String[] args) {
-		int x = 40, y = 30;
+//		int x = 10, y = 20;
+//
+//		boolean test = true;
+//
+//		if (test) {
+//			System.out.println("Test passato");
+//		} else {
+//			System.out.println("Test non passato");
+//		}
+//
+//		if (x > y) {
+//			System.out.println("X maggiore di Y");
+//		} else if (x == 10) {
+//			System.out.println("X uguale a 10");
+//		} else {
+//			System.out.println("X non è maggiore di Y o uguale a 10");
+//		}
+//
+//		if (y % 2 == 0) {
+//			System.out.println("Il numero è pari");
+//		}
+//
+//		System.out.println(x <= y ? "minore" : "maggiore");
+//
+//		System.out.println(getValue(10, 20, 30));
 
-		boolean test = true;
+//		String favouriteColour = "red";
+//		switch (favouriteColour) {
+//		case "red": {
+//			System.out.println("Il colore è rosso");
+//			break;
+//		}
+//		case "blue": {
+//			System.out.println("Il colore è blue");
+//			break;
+//		}
+//		case "green": {
+//			System.out.println("Il colore è verde");
+//			break;
+//		}
+//		default:
+//			System.out.println("Nessuno dei precedenti");
+//		}
 
-		if (test) {
-			System.out.println("Test passato");
-		} else {
-			System.out.println("Test non passato");
-		}
+//		int x = 10, y = 20;
+//		int z = (x > y) ? -100 : 123;
+//
+//		switch (z) {
+//		default:
+//			System.out.println("red");
+//			--x;
+//			break;
+//		case 30:
+//			x++;
+//			break;
+//		case -100:
+//			System.out.println("blue");
+//		case 100:
+//			System.out.println("green");
+//			--x;
+//			// break;
+//		case 1000:
+//			System.out.println("black");
+//		}
+//
+//		System.out.println(x);
 
-		if (x > y) {
-			System.out.println("X maggiore di Y");
-		} else if (x == 10) {
-			System.out.println("X uguale a 10");
-		} else {
-			System.out.println("X non è maggiore di Y o uguale a 10");
-		}
+		// *********************************** LOOPS
+		// ***************************************
 
-		if (y % 2 == 0) {
-			System.out.println("Il numero è pari");
-		}
+		int x = 6, y = 3;
 
-		System.out.println(x <= y ? "minore" : "maggiore");
+//		while (y <= x + 6) {
+//			System.out.println(y);
+//			y += 3;
+//		}
 
-		System.out.println(getValue(10, 20, 30));
+//		do {
+//			System.out.println(x++);
+//		} while (x < 10);
+
+//		for (int i = 1; i <= 10; i++) {
+//			if (i % 2 == 0)
+//				continue;
+//			System.out.println(i);
+//		}
+
+//		for (int i = 1; i <= 10; i++) {
+//			if (i == 5)
+//				break;
+//			System.out.println(i);
+//		}
+
 	}
 
 	static int getValue(int a, int b, int c) {
